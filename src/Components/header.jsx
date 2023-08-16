d0a6a49e8a00eef564cd4472af37a66c4e7c9f71
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-08-16 09:56:36 +0100

    Heading

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,7 +1,7 @@
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
-import Logo from '../Assets/Images/Logo.png'
+import Logo from "../Assets/Images/Logo.png";
 
 const navigation = [
   { name: "Individual", href: "#" },
@@ -16,18 +16,24 @@ export default function Header() {
   return (
     <header className="absolute inset-x-0 top-0 z-50">
       <nav
-        className="flex items-center justify-between p-6 lg:px-8"
+        className="flex w-4/5 m-auto grid grid-cols-3 gap-4 items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              className="h-8 w-auto"
-              src={Logo}
-              alt="Logo"
-            />
+        <div className="flex lg:flex-1 col-span-2 items-center">
+          <a href="#" className="-m-1.5 mr-4 p-1.5">
+            <img className="h-8 w-auto" src={Logo} alt="Logo" />
           </a>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-base leading-2 text-gray-900 font-body hover:text-secondary"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -39,24 +45,14 @@ export default function Header() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              {item.name}
-            </a>
-          ))}
-        </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-           Register <span aria-hidden="true">&rarr;</span>
-          </a>
+
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end font-body grid grid-cols-3 gap-4">
+          <button className="rounded-full bg-none border border-primary w-1/2 text-primary p-3">
+            Login
+          </button>
+          <button className="rounded-full bg-primary text-light w-1/2 p-3">
+            Register
+          </button>
         </div>
       </nav>
       <Dialog
@@ -69,10 +65,9 @@ export default function Header() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                src={Logo}
                 alt=""
               />
             </a>
