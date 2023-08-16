e7c9851a5554ab0403122c0f815c92da6fcb8bc1
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-08-16 09:56:55 +0100

    Heading

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#11453B",
+      dark: "#292A29",
+      light: "#ffffff",
+      secondary: "#EA4E4B",
+      invert: "#B4A572",
+    },
     fontFamily: {
-      headings: ["MADE Mellow"],
-      body: ["Aeonik"],
+      headingBlack: ["MadeMellowBlack"],
+      headingBold: ["MadeMellowBold"],
+      headingLight: ["MadeMellowLight"],
+      headingMedium: ["MadeMellowMedium"],
+      headingReg: ["MadeMellowRegular"],
+      headingSB: ["MadeMellowSemibold"],
+      body: ["AeonikRegular"],
+      bodyBold: ["Aeonik"],
+      bodyLight: ["AeonikLight"],
     },
     extend: {},
   },
