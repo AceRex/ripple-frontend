0c03c0519acc7d153d01b9fbc80a9dbac98f7279
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-08-16 07:29:42 +0100

    Push

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// @type {import('tailwindcss').Config}
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      headings: ["MADE Mellow"],
+      body: ["Aeonik"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
