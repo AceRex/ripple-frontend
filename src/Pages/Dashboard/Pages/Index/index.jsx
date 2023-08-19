4e0de4046a37fc125de84e0dae2f5d564d1fb230
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-08-19 09:23:55 +0100

    push

--- src/Pages/Dashboard/Pages/Index/index.jsx
+++ src/Pages/Dashboard/Pages/Index/index.jsx
@@ -1,11 +1,9 @@
 import Top from "./Top";
-import Graph from "./Graph";
 
 export default function Index() {
   return (
     <section className="px-12 pt-10 text-dark">
       <Top />
-      {/* <Graph /> */}
     </section>
   );
 }
