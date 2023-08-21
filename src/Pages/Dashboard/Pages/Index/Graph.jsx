30fde5a0b81c3ab261c981dd21ba2c7bb35f2e1d
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-08-21 21:43:54 +0100

    push

--- src/Pages/Dashboard/Pages/Index/Graph.jsx
+++ src/Pages/Dashboard/Pages/Index/Graph.jsx
@@ -20,24 +20,43 @@ ChartJs.register(
 
 export default function Graph() {
   const data = {
-    labels: ["Q1", "Q2", "Q3"],
+    labels: [
+      "JAN",
+      "FEB",
+      "MAR",
+      "APR",
+      "MAY",
+      "JUN",
+      "JUL",
+      "AUG",
+      "SEP",
+      "OCT",
+      "NOV",
+      "DEC",
+    ],
     datasets: [
       {
         label: "2020",
-        data: [20000, 300000, 6000000],
+        data: [
+          20000, 300000, 6000000, 20000, 300000, 6000000, 20000, 300000,
+          6000000, 20000, 300000, 6000000,
+        ],
         backgroundCOlor: "red",
         borderColor: "aqua",
         pointBorderColor: "red",
-        tension: 0.4,
+        tension: 1,
       },
       {
         label: "2021",
-        data: [10000, 7000000, 3000000],
+        data: [
+          10000, 7000000, 3000000, 20000, 300000, 6000000, 20000, 300000,
+          6000000, 10000, 7000000, 3000000,
+        ],
         backgroundCOlor: "green",
         borderColor: "red",
         pointBorderColor: "green",
         fill: true,
-        tension: 0.4,
+        tension: 1,
       },
     ],
   };
@@ -47,7 +66,7 @@ export default function Graph() {
     },
     scales: {
       y: {
-        min: 10000,
+        min: 0,
         max: 10000000,
       },
     },
